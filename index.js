53000103971067a98b18d46d5cf9ebefe4209262
Adrian <adrian@mail.com>
2023-01-12 09:15:07 +0100

    primero

---
+++ index.js
@@ -0,0 +1,5 @@
+import { suma, resta, autor } from "./modulos/operaciones.js"
+
+console.log(autor);
+
+console.log(resta(3, 2));
---
+++ modulos/operaciones.js
@@ -0,0 +1,15 @@
+function suma(n1, n2){
+    return n1 + n2;
+}
+
+function resta(n1, n2){
+    return n1 - n2;
+}
+
+const autor = 'Adrián Benítez';
+
+export { 
+    suma,
+    resta,
+    autor 
+}
